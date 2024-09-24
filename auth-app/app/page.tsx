e290e5ef2ef162d2718b0d9b918073483e14ad69
Filipsys <filipsysak@gmail.com>
2024-09-25 00:19:27 +0200

    Initial commit

---
+++ auth-app/app/page.tsx
@@ -0,0 +1,25 @@
+import { auth } from "@/auth";
+import SignIn from "./components/sign-in";
+import SignOut from "./components/sign-out";
+
+export default async function Home() {
+  const session = await auth();
+  if (!session?.user) {
+    return <SignIn />;
+  }
+
+  return (
+    <div className="flex flex-col h-screen w-screen">
+      <div className="h-16 bg-slate-400 flex w-screen items-center">
+        {session.user ? <SignOut /> : <SignIn />}
+      </div>
+      <div className="w-full h-full bg-slate-200 flex justify-center items-center text-3xl font-bold flex-col">
+        <p>
+          {session.user
+            ? `Hello ${session.user.name}`
+            : "You are not signed in"}
+        </p>
+      </div>
+    </div>
+  );
+}
